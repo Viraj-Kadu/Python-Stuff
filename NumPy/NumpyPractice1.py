b76717f85a0e93e6fe1d407e2626b3a52400d47b
Viraj-Kadu <vkadu9999@gmail.com>
2024-10-06 22:02:11 +0530

    Add files via upload

---
+++ NumPy/NumpyPractice1.py
@@ -0,0 +1,7 @@
+# Problem 1:
+# Write a NumPy program to find the dot product of two arrays of different dimensions.
+import numpy as np
+import random
+a = np.array([[1, 2],[3,4],[4,5]])
+b = np.array([5, 6])
+print(np.dot(a,b))
